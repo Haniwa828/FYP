004c782d59327836ef2f4251246f1129d142e21a
Haniwa828 <99132172+Haniwa828@users.noreply.github.com>
2023-04-26 08:11:26 +0800

    秒数上限解放

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ st.title('Map Generate from UAV Movie')
 st.sidebar.write('Resource and Parameta')
 
 input_file = st.sidebar.file_uploader('Upload the movie', type=['mp4'])
-interval = st.sidebar.slider('Interval seconds (0.x sec)', min_value = 5, max_value = 10, value = 5)/10
+interval = st.sidebar.slider('Interval seconds (0.x sec)', min_value = 5, max_value = 20, value = 5)/10
 angle = st.sidebar.slider('Angle', min_value = 0, max_value = 89, value = 0)
 
 
